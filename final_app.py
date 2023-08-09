1431dc0e36df24651e511a1c4efea63b65476ae2
dapniii <daphne_louise_ong@dlsu.edu.ph>
2023-08-09 16:54:21 +0800

    Update navbar to bootstrap version

--- final_app.py
+++ final_app.py
@@ -2,27 +2,29 @@ import dash
 from dash import Dash, html, dcc, Input, Output, callback
 import dash_bootstrap_components as dbc
 
-
 # Initialize Dash application
 app = Dash(__name__, 
            use_pages=True,
            external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
-    # Change to bootstrap navbar
-    html.Div(
-        [
-            html.Div(
-                dcc.Link(
-                    f"{page['name']} - {page['path']}", href=page["relative_path"]
-                )
-            )
+    # Bootstrap navbar 
+    dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dbc.NavLink(f"{page['name']}", href=page["relative_path"]))
             for page in dash.page_registry.values()
-        ]
+        ],
+        brand="Global Terrorism",
+        brand_href="#",
+        color="dark",
+        dark=True,
+        style={
+            'margin-bottom': '1em'
+        }
     ),
 	dash.page_container
 ])
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', debug=True)
+    app.run_server(debug=True)
